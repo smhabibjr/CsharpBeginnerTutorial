43dfec7d4d17bc01d0f00e6ec7d30865a1ca1d77
HabibJR <smhabib.abir2030@gmail.com>
2022-07-30 23:13:32 +0200

    user input and print that

--- CsharpBeginnerTutorial/Program.cs
+++ CsharpBeginnerTutorial/Program.cs
@@ -6,7 +6,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // user input and print that on terminal
+
+            Console.WriteLine("Enter your user name ");
+            string user_name =  Console.ReadLine();
+            Console.WriteLine("Enter your age ");
+            int user_age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your user name is " + user_name + " And You are " +  user_age + " Years old !");
         }
     }
 }
