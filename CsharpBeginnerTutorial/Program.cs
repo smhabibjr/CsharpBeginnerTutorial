f0bb58188581df7b0e1f679aefcd8176452346f9
HabibJR <smhabib.abir2030@gmail.com>
2022-07-30 22:14:48 +0200

    data type and data casting

--- CsharpBeginnerTutorial/Program.cs
+++ CsharpBeginnerTutorial/Program.cs
@@ -6,7 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int myNum = 5;               // Integer (whole number)
+            double myDoubleNum = 5.99D;  // Floating point number
+            char myLetter = 'D';         // Character
+            bool myBool = true;          // Boolean
+            string myText = "Hello";     // String
+
+            //Implicit Casting
+            // char > int > lang > float > double
+            double myDouble = myNum;
+            Console.WriteLine("Implicit casting " + myDouble.GetType());
+
+            //Type Conversion Methods
+            string convert_int_to_string = Convert.ToString(myNum);
+            // more conversion methods 
+            Console.WriteLine(Convert.ToString(myNum));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myNum));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myNum));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));
+            Console.WriteLine("Type Conversion" + convert_int_to_string.GetType());
         }
     }
 }
